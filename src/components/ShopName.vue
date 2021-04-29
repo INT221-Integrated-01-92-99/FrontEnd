ed30ed639e3e1d147c4f944c0f98d91bf47a8369
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-04-29 15:14:25 +0700

    [Add] BaseButton, BaseCard and ShopName components

---
+++ src/components/ShopName.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1 class="font-serif font-medium 2xl:text-7xl xl:text-6xl text-5xl text-white tracking-widest">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShopName',
+  props: {
+    msg: String
+  }
+}
+</script>
