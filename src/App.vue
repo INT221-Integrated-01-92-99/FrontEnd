3165dec0e8f1b643ee567e6aefa7d68e0aba29cb
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-04-29 15:16:02 +0700

    [Add] global register components and router-link

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav
+    :class="{ 'bg-opacity-0 shadow-none': routes == '/' }"
+    class="navbar fixed flex justify-end w-full 2xl:space-x-10 xl:space-x-9 space-x-8 2xl:p-3 xl:p-3 p-3 2xl:pr-20 xl:pr-20 pr-20 top-0 text-white bg-cream-dark shadow-md"
+  >
+    <!-- <div class="mr-96">
+    <router-link
+      @click="changePath('/')"
+      tag="li"
+      to="/"
+      class="flex justify-start tracking-widest 2xl:text-xl text-base font-serif font-semibold mr-96 pr-56"
+      ><img class="h-10 w-10" src="./assets/paypal-logo.svg"></router-link
+    ></div> -->
+    <router-link
+      @click="changePath('/')"
+      tag="li"
+      to="/"
+      class="2xl:text-base text-base font-serif font-medium transition ease-in duration-300 transform hover:-translate-y-1 active:translate-y-0 p-2 focus:text-cream-light-brown hover:text-cream-light-brown"
+      >Home</router-link
+    >
+    <router-link
+      @click="changePath('/team')"
+      tag="li"
+      to="/team"
+      class="2xl:text-base text-base font-serif font-medium transition ease-in duration-300 transform hover:-translate-y-1 active:translate-y-0 p-2 focus:text-cream-light-brown hover:text-cream-light-brown"
+      >Team</router-link
+    >
+    <router-link
+      @click="changePath('/product')"
+      to="/product"
+      class="2xl:text-base text-base font-serif font-medium transition ease-in duration-300 transform hover:-translate-y-1 active:translate-y-0 p-2 focus:text-cream-light-brown hover:text-cream-light-brown"
+      >Product</router-link
+    >
+  </nav>
+  <router-view />
 </template>
 
-<style>
+<script>
+export default {
+  data() {
+    return {
+      routes: "/",
+    };
+  },
+  methods: {
+    changePath(path) {
+      this.routes = path;
+    },
+  },
+};
+</script>
 
+<style>
+nav {
+  z-index: 10;
+}
+
+nav.scrolled {
+  @apply shadow-2xl;
+  border-bottom: 0px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import BaseCard from './components/BaseCard.vue'
+import BaseButton from './components/BaseButton.vue'
+import BaseForm from './components/BaseForm.vue'
+import BaseProductHorizontal from './components/BaseProductHorizontal.vue'
 
-createApp(App).use(router).mount('#app')
+
+const app = createApp(App).use(router)
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+app.component('base-form', BaseForm)
+app.component('base-product-horizontal', BaseProductHorizontal)
+app.mount('#app')
