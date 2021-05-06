0de57bcd0ea4c9f20477ba0d15b7faf10953a42d
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-05-07 02:25:54 +0700

    [Update] change component name

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import './assets/tailwind.css'
 import BaseCard from './components/BaseCard.vue'
 import BaseButton from './components/BaseButton.vue'
 import BaseForm from './components/BaseForm.vue'
-import BaseProductHorizontal from './components/BaseProductHorizontal.vue'
-
+import BaseProductView from './components/BaseProductView.vue'
 
 const app = createApp(App).use(router)
 app.component('base-card', BaseCard)
 app.component('base-button', BaseButton)
 app.component('base-form', BaseForm)
-app.component('base-product-horizontal', BaseProductHorizontal)
+app.component('base-product-view', BaseProductView)
 app.mount('#app')
