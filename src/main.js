576909e9944570c2423e72f5c1e7061d8d58fdb4
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-05-10 16:26:03 +0700

    [Update] regist global component BaseDelete

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import BaseCard from './components/BaseCard.vue'
 import BaseButton from './components/BaseButton.vue'
 import BaseForm from './components/BaseForm.vue'
 import BaseProductView from './components/BaseProductView.vue'
+import BaseDelete from './components/BaseDelete.vue'
 
 const app = createApp(App).use(router)
 app.component('base-card', BaseCard)
 app.component('base-button', BaseButton)
 app.component('base-form', BaseForm)
 app.component('base-product-view', BaseProductView)
+app.component('base-delete', BaseDelete)
 app.mount('#app')
